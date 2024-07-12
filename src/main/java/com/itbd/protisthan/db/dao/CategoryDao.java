9f1bfd44c7b0a5f6be3249188a1fce1b1d54f453
ridoy.kumar <ridoy.kumar@gmail.com>
2024-07-12 17:58:26 +0600

    feat: Add CategoryDao class for managing categories in the database

---
+++ src/main/java/com/itbd/protisthan/db/dao/CategoryDao.java
@@ -0,0 +1,31 @@
+package com.itbd.protisthan.db.dao;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "categories", schema = "erp_over")
+public class CategoryDao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_category_key", nullable = false)
+    private Long id;
+
+    @Size(max = 15)
+    @NotNull
+    @Column(name = "tx_category_name", nullable = false, length = 15)
+    private String categoryName;
+
+    @Lob
+    @Column(name = "tx_picture")
+    private String picture;
+
+    @Lob
+    @Column(name = "tx_description")
+    private String description;
+}
