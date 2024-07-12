5688c60b6985d45c5d5985eff47ca689c2724d10
ridoy.kumar <ridoy.kumar@gmail.com>
2024-07-12 18:39:36 +0600

    feat: Add ProtisthanApplicationTest for testing the application context

---
+++ src/test/java/com/itbd/protisthan/ProtisthanApplicationTest.java
@@ -0,0 +1,12 @@
+package com.itbd.protisthan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProtisthanApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+
+}
