39e522486fa1e5291d0c575068ce396d75eb1924
ridoy.kumar <ridoy.kumar@gmail.com>
2024-07-12 18:04:21 +0600

    feat: Add EnvProp, AuditorAwareImpl, ProtisthanApplication, AppConfig, and MariaDBConfig classes

---
+++ src/main/java/com/itbd/protisthan/config/EnvProp.java
@@ -0,0 +1,26 @@
+package com.itbd.protisthan.config;
+
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.hilla.Endpoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+
+@Endpoint
+@AnonymousAllowed
+public class EnvProp {
+
+    @Autowired
+    private Environment env;
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
+    public String[] getProp() {
+        return this.env.getActiveProfiles();
+    }
+
+    public String getActiveProfile() {
+        return this.activeProfile;
+    }
+}
