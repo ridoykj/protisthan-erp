fd551e7bcd4fa0ca6a82d2422938fd93e7f71fb6
ridoy.kumar <ridoy.kumar@gmail.com>
2024-07-12 18:05:49 +0600

    feat: Add AdministratorBaseView and FromView components for the Administrator module

---
+++ src/main/frontend/views/module/administrator/@index.tsx
@@ -0,0 +1,20 @@
+import { ViewConfig } from '@vaadin/hilla-file-router/types.js';
+import React from 'react';
+
+export const config: ViewConfig = {
+  menu: {
+    title: 'Administrator',
+    icon: 'users',
+  },
+};
+
+const AdministratorBaseView: React.FC = () => {
+  return (
+    <div className="flex bg-green-800 justify-center">
+      <h1>User Page </h1>
+      <p> This is a simple user page. </p>
+    </div>
+  );
+};
+
+export default AdministratorBaseView;
