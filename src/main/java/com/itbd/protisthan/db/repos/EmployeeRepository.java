04a03ff616b1c1210fc36d22b356efe56cf0f2f4
ridoy.kumar <ridoy.kumar@gmail.com>
2024-07-12 18:32:44 +0600

    feat: Add EmployeeRepository interface for managing employee details

---
+++ src/main/java/com/itbd/protisthan/db/repos/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.itbd.protisthan.db.repos;
+
+import com.itbd.protisthan.db.dao.EmployeeDao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface EmployeeRepository extends JpaRepository<EmployeeDao, Long>, JpaSpecificationExecutor<EmployeeDao> {
+}
